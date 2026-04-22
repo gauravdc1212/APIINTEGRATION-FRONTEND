2448c1e6ae720e3a10e0eb10fcba85e4f2e16b90
Gaurav Kumar Singh <gaurav676590@gmail.com>
2026-04-22 16:19:04 +0530

    updated baseURL

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000/api",
+  // baseURL: "http://localhost:7000/api",
+  baseURL:"https://piintegration-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
